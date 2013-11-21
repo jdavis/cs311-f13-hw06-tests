5d821225ce0281bdddd06aab21d5327bcb20389e
Josh Davis <josh@joshldavis.com>
2013-11-20 19:04:25 -0600

    Add MaxFlow creator

--- TestRunner.java
+++ TestRunner.java
@@ -15,7 +15,7 @@ import edu.iastate.cs311.f13.hw6.*;
  */
 public final class TestRunner {
     /**
-     * Create a new instance of the above class automatically.
+     * Create a new instance of the IGraph class automatically.
      * @return An instance that adheres to the IGraph interface
      *
      * Replace `Graph` class with the name of your implementation of the IGraph
@@ -27,7 +27,9 @@ public final class TestRunner {
     }
 
     /**
-     * Create a new instance of the above class automatically.
+     * Create a new instance of the ITopologicalSortAlgorithms class
+     * automatically.
+     *
      * @return An instance that adheres to the ITopologicalSort interface
      *
      * Replace `TopologicalSort` class with the name of your implementation of the
@@ -35,6 +37,19 @@ public final class TestRunner {
      *
      */
     public static ITopologicalSortAlgorithms newTopoSort() {
-        return new TopologicalSort();
+        return new TopologicalSortAlgorithms();
+    }
+
+    /**
+     * Create a new instance of the IMaxFlowAlgorithms class automatically.
+     *
+     * @return An instance that adheres to the ITopologicalSort interface
+     *
+     * Replace `TopologicalSort` class with the name of your implementation of the
+     * ITopologicalSort interface.
+     *
+     */
+    public static IMaxFlowAlgorithms newMaxFlow() {
+        return new MaxFlowAlgorithms();
     }
 }
